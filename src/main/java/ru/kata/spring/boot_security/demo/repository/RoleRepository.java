9949d0a059b4d6eed4745ea8be00b4bd47939161
slavk <slavka.cherenkov@gmail.com>
2025-03-13 19:46:49 +0300

    commit!!!!

---
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
@@ -0,0 +1,17 @@
+package ru.kata.spring.boot_security.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.kata.spring.boot_security.demo.models.Role;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByRole(String roleName);
+
+    List<Role> findByIdIn(List<Long> ids);
+
+}
--- src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImpl.java
@@ -1,9 +1,7 @@
 package ru.kata.spring.boot_security.demo.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 
